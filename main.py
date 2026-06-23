03cfbc6f55f190ed5939c5cd6da4f1847d57f5b6
rclind <rclind@users.noreply.github.com>
2026-06-23 04:06:57 +0000

    Finished Initial Training

--- main.py
+++ main.py
@@ -76,15 +76,15 @@ def go(config: DictConfig):
 
         if "data_split" in active_steps:
             _ = mlflow.run(
-        os.path.join(hydra.utils.get_original_cwd(), "src", "data_split"),
-        "main",
-        parameters={
-            "input_artifact": "clean_sample.csv:latest",
-            "test_size": config["modeling"]["test_size"],
-            "random_seed": config["modeling"]["random_seed"],
-            "stratify_by": config["modeling"]["stratify_by"],
-        },
-    )
+                f"{config['main']['components_repository']}/train_val_test_split",
+                "main",
+                parameters={
+                    "input": "clean_sample.csv:latest",
+                    "test_size": config["modeling"]["test_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"],
+                },
+        )
 
         if "train_random_forest" in active_steps:
 
@@ -93,19 +93,18 @@ def go(config: DictConfig):
             with open(rf_config, "w+") as fp:
                 json.dump(dict(config["modeling"]["random_forest"].items()), fp)  # DO NOT TOUCH
 
-            
             _ = mlflow.run(
-               os.path.join(hydra.utils.get_original_cwd(), "src", "train_random_forest"),
-              "main",
-              parameters={
-                  "trainval_artifact": "trainval_data.csv:latest",
-                  "val_size": config["modeling"]["val_size"],
-                  "random_seed": config["modeling"]["random_seed"],
-                  "stratify_by": config["modeling"]["stratify_by"],
-                  "max_tfidf_features": config["modeling"]["max_tfidf_features"],
-                  "rf_config": rf_config,
-                  "output_artifact": "random_forest_export",
-            },
+                os.path.join(hydra.utils.get_original_cwd(), "src", "train_random_forest"),
+                "main",
+                parameters={
+                    "trainval_artifact": "trainval_data.csv:latest",
+                    "val_size": config["modeling"]["val_size"],
+                    "random_seed": config["modeling"]["random_seed"],
+                    "stratify_by": config["modeling"]["stratify_by"],
+                    "max_tfidf_features": config["modeling"]["max_tfidf_features"],
+                    "rf_config": rf_config,
+                    "output_artifact": "random_forest_export",
+                },
         )
 
         if "test_regression_model" in active_steps:
